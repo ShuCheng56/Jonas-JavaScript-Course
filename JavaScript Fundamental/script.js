049c2be1437127bf290153c2f4dbe807298376ec
ShuCheng56 <97557132+ShuCheng56@users.noreply.github.com>
2022-12-09 14:15:08 -0800

    Add files via upload

---
+++ JavaScript Fundamental/script.js
@@ -0,0 +1,53 @@
+// let js = 'amazing';
+//       if ( js === 'amazing') alert ('JavaScript is Fun!');
+
+//       console.log(40 + 8 + 23 - 10);
+
+// let country = 'P.R.China';
+// const continent = 'Asia';
+// let ChinaPopulation = '14130000001';
+// let FinlandPopulation = '6000000';
+// let AveragePopulation = '3300000';
+
+// console.log(country);
+// console.log(continent);
+// console.log(ChinaPopulation);
+
+
+// const isIsland = 'CN';
+// const language = 'mandarin';
+// let description = `China is in Asia,and its ${ChinaPopulation} people speak portuguese`;
+
+
+// console.log(isIsland);
+// console.log(ChinaPopulation);
+// console.log(country);
+// console.log(language);
+// console.log(description);
+
+
+// // Basic Operators 
+
+// console.log(ChinaPopulation / 2);
+// console.log(ChinaPopulation > FinlandPopulation);
+// console.log(ChinaPopulation < AveragePopulation);
+
+
+// JavaScript Fundamentals  Challengs Part 1
+
+
+let MarksMass = 95;
+let MarksHeight = 1.88;
+let JohnMass = 85;
+let JohnHeight = 1.76;
+
+
+
+const MarksBMI = MarksMass / (MarksHeight * MarksHeight);
+const JohnBMI = JohnMass / (JohnHeight * JohnHeight);
+let MarkHigherBMI = MarksBMI > JohnBMI;
+
+console.log(MarksBMI, JohnBMI);
+
+console.log(MarkHigherBMI);
+
